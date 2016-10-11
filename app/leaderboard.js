85648c30bb4c3fa96a9ae38b01e3e7071313293d
Egan Nakano <enakano@hawaii.edu>
2016-10-11 12:57:30 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -13,18 +13,19 @@ console.log('Hello Client');
       return PlayersList.find();
     },
     'selectedClass': function(){
-      var playerId = this._id;
-      var selectedPlayer = Session.get('selectedPlayer');
+      let playerId = this._id;
+      let selectedPlayer = Session.get('selectedPlayer');
       if(playerId == selectedPlayer){
         return "selected"
+      }
     }
   });
 
   Template.leaderboard.events({
     'click  .player': function(){
-      var playerId = this._id;
+      let playerId = this._id;
       Session.set('selectedPlayer', playerId);
-      var selectedPlayer = Session.get('selectedPlayer');
+      let selectedPlayer = Session.get('selectedPlayer');
       console.log(selectedPlayer);
     }
   });
