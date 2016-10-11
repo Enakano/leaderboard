dac047d9d1d75e827d8d20d67fab4138fd976426
Egan Nakano <enakano@hawaii.edu>
2016-10-11 10:17:55 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,4 +1,35 @@
 /**
  * Created by Egan on 10/11/2016.
  */
-console.log("Hello world");
+
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+
+console.log('Hello Client');
+
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+    }
+  });
+
+  Template.leaderboard.events({
+    'click  .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+      var selectedPlayer = Session.get('selectedPlayer');
+      console.log(selectedPlayer);
+    }
+  });
+}
+
+if(Meteor.isServer){
+  console.log("Hello Server");
+}
